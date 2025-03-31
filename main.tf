8bbceec527ed847bee507f6244be75a96fc73131
Jagannnnnn <jaganmohan85220@gmail.com>
2025-03-31 16:54:23 +0530

    learn ansible

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "instance" {
   count = length(var.instances)
   ami     = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group_ids
+  # vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = var.instances[count.index]
