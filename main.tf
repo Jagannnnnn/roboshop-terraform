752de21bad2e37405f767dfd4171c5dfcaf1b403
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-01 19:55:43 +0530

    learn ansible

--- main.tf
+++ main.tf
@@ -1,24 +1,16 @@
-resource "aws_instance" "instance" {
-for_each = var.instances
-ami     = each.value["ami_id"]
-  instance_type = each.value["instance_type"]
-  # vpc_security_group_ids = var.vpc_security_group_ids
-
-  tags = {
-    Name = each.key
-  }
-
-}
-
-
-resource "aws_route53_record" "record" {
+module "ec2" {
   for_each = var.instances
+  source = "./modules/ec2"
+
+  ami_id = each.value["ami_id"]
+  env    = var.env
+  instance_type = each.value["instance_type"]
+  name = each.key
   zone_id = var.zone_id
-  name    = "${each.key}-${var.env}"
-  type    = "A"
-  ttl     = 10
-  records = [aws_instance.instance[each.key].private_ip]
+
+
 }
+
 
 # resource "null_resource" "catalogue" {
 #   provisioner "remote-exec" {
---
+++ modules/ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "instance" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  # vpc_security_group_ids = var.vpc_security_group_ids
+
+  tags = {
+    Name = var.name
+  }
+
+}
+
+
+resource "aws_route53_record" "record" {
+  zone_id = var.zone_id
+  name    = "${var.name}-${var.env}"
+  type    = "A"
+  ttl     = 10
+  records = [aws_instance.instance.private_ip]
+}
