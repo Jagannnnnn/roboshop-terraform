8f79476857333d9a775d60b66dbf1bd08c3ed003
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-01 20:16:15 +0530

    learn ansible

---
+++ env-dev/main.tfvars
@@ -0,0 +1,18 @@
+instances = {
+  frontend = {
+    ami_id = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+  }
+  catalogue = {
+    ami_id = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+
+  }
+  mongodb = {
+    ami_id = "ami-09c813fb71547fc4f"
+    instance_type = "t3.small"
+  }
+}
+
+zone_id = "Z07420703D2F2QO6YXFHV"
+env = "dev"
--- variables.tf
+++ variables.tf
@@ -1,28 +1,6 @@
 variable "vpc_security_group_ids" {
   default = "sg-09a30c40cfd8547e6"
 }
-variable "instances" {
-  default = {
-    frontend = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-    }
-    catalogue = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.micro"
-
-    }
-    mongodb = {
-      ami_id = "ami-09c813fb71547fc4f"
-      instance_type = "t3.small"
-    }
-
-  }
-}
-
-variable "zone_id" {
-  default = "Z07420703D2F2QO6YXFHV"
-}
-variable "env" {
-  default = "dev"
-}
+variable "instances" {}
+variable "zone_id" {}
+variable "env" {}
