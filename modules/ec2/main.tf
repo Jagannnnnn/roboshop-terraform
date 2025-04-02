93f920409957f090c0af47b75974e8bec2f3b912
Jagannnnnn <jaganmohan85220@gmail.com>
2025-04-02 21:53:29 +0530

    learn ansible

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
+  }
+}
 resource "aws_instance" "instance" {
   ami = var.ami_id
   instance_type = var.instance_type
@@ -17,3 +25,20 @@ resource "aws_route53_record" "record" {
   ttl     = 10
   records = [aws_instance.instance.private_ip]
 }
+
+resource "null_resource" "catalogue" {
+   provisioner "remote-exec" {
+
+     connection {
+       type     = "ssh"
+       user     = "ec2-user"
+       password = "DevOps321"
+       host     = aws_instance.instance.private_ip
+     }
+     inline = [
+       " sudo pip3.11 install ansible",
+       "ansible-pull -i localhost, -U https://github.com/Jagannnnnn/roboshop-ansible2.git roboshop.yml -e component_name=$(var.name) -e env=$(var.env)",
+     ]
+    }
+
+ }
